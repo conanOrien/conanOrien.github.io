18c150e82162d1edee74e3a8072a9bb095233982
Spencer Gunning <swcgunning@gmail.com>
2017-12-06 14:46:54 -0500

    [2017r2] Added navbar responsivity

--- home.js
+++ home.js
@@ -1,8 +1,2 @@
 // BEGIN ALL-PAGE JS //
-
-function myFunction() {
-    nav = $('#nav');
-    nav.toggleClass('responsive', !nav.hasClass('responsive'));
-}
-
-//  END ALL-PAGE JS  //
+//  END ALL-PAGE JS //
