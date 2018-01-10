0e9a9b9c39a03c2b95869f359a24b58abd389a0a
Spencer Gunning <swcgunning@gmail.com>
2018-01-10 10:32:49 -0500

    [2018r1] Added simple dropdown

--- home.js
+++ home.js
@@ -1,2 +1,25 @@
 // BEGIN ALL-PAGE JS //
+
+shows_on = false;
+
+$(document).ready(function () {
+    $("html").click(function(e) {
+        if (shows_on && e.target.nodeName != "A") {
+            $("#dl_shows").slideUp(function() {
+                $("#dl_shows").removeClass("show");
+                shows_on = false
+            });
+        };
+    });
+
+    $("#dd_shows").click(function() {
+        if (!shows_on) {
+            $("#dl_shows").slideDown(function() {
+                $("#dl_shows").addClass("show");
+                shows_on = true
+            });
+        }
+    });
+});
+
 //  END ALL-PAGE JS //
