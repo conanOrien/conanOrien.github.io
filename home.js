54541de7357e07300c9aad7326d31bfe0187b1c3
Spencer Gunning <swcgunning@gmail.com>
2018-01-11 21:46:09 -0500

    [2018r4] Finished navtab functionality

--- home.js
+++ home.js
@@ -2,38 +2,45 @@
 
 var navtab_active = null;
 var anchor_queued = null;
+var click_enabled = true;
 
 function navtab_id(e) {
     var anchor_id = e.toString();
     anchor_queued = anchor_id.replace("a_", "#nt_");
 };
 
-function reveal(nt) {
-    $(nt + " a").hide().delay(150).fadeIn(600);
-    $(nt).slideDown(750, function() {
-        navtab_active = nt;
-        anchor_queued = null;
-    });
-};
-
-function hide(nt) {
-    $(nt + " a").show().fadeOut(600);
-    $(nt).slideUp(750, function() {
-        navtab_active = null;
-    });   
-};
-
 $(document).ready(function () {
-    $("html").click(function(e) {
-        if (navtab_active == null) {
-            reveal(anchor_queued);
-        } else if (anchor_queued == null && e.target.nodeName != "A") {
-            hide(navtab_active);
-        } else if (anchor_queued != null && anchor_queued != navtab_active) {
-            hide(navtab_active);
-            reveal(anchor_queued);
-        } else {
-            anchor_queued = null;
+    $("html").click(function (e) {
+        if (click_enabled) {
+            click_enabled = false;
+            if (navtab_active == null) {
+                $(anchor_queued + " a").hide().delay(150).fadeIn(600);
+                $(anchor_queued).slideDown(750, function() {
+                    navtab_active = anchor_queued;
+                    anchor_queued = null;
+                    click_enabled = true;
+                });
+            } else if (anchor_queued == null && e.target.nodeName != "A") {
+                $(navtab_active + " a").show().fadeOut(400);
+                $(navtab_active).slideUp(550, function() {
+                    navtab_active = null;
+                    click_enabled = true;
+                });   
+            } else if (anchor_queued != null && anchor_queued != navtab_active) {
+                $(navtab_active + " a").show().fadeOut(400);
+                $(navtab_active).slideUp(550, function() {
+                    navtab_active = null;
+                    $(anchor_queued + " a").hide().delay(150).fadeIn(600);
+                    $(anchor_queued).slideDown(750, function() {
+                        navtab_active = anchor_queued;
+                        anchor_queued = null;
+                        click_enabled = true;
+                    });
+                });
+            } else {
+                anchor_queued = null;
+                click_enabled = true;
+            };
         };
     });
 });
