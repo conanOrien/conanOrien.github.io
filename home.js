729694b6c0a9cd72aaeff557456d51fe5029d0c1
Spencer Gunning <swcgunning@gmail.com>
2018-01-12 09:58:48 -0500

    [2018r5] Last changes before implementing new CSS

--- home.js
+++ home.js
@@ -5,15 +5,17 @@ var anchor_queued = null;
 var click_enabled = true;
 
 function navtab_id(e) {
-    var anchor_id = e.toString();
-    anchor_queued = anchor_id.replace("a_", "#nt_");
+    if (click_enabled) {
+        var anchor_id = e.toString();
+        anchor_queued = anchor_id.replace("a_", "#nt_");
+    };
 };
 
 $(document).ready(function () {
     $("html").click(function (e) {
         if (click_enabled) {
             click_enabled = false;
-            if (navtab_active == null) {
+            if (navtab_active == null && anchor_queued != null) {
                 $(anchor_queued + " a").hide().delay(150).fadeIn(600);
                 $(anchor_queued).slideDown(750, function() {
                     navtab_active = anchor_queued;
@@ -26,7 +28,8 @@ $(document).ready(function () {
                     navtab_active = null;
                     click_enabled = true;
                 });   
-            } else if (anchor_queued != null && anchor_queued != navtab_active) {
+            } else if (anchor_queued != null &&
+            anchor_queued != navtab_active) {
                 $(navtab_active + " a").show().fadeOut(400);
                 $(navtab_active).slideUp(550, function() {
                     navtab_active = null;
