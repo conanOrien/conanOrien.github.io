bdac6d9c2a6a1f7e82ddf47306bc0d85f47f3a68
Spencer Gunning <swcgunning@gmail.com>
2018-01-18 12:04:44 -0500

    [2018r15] Completion of temporary stream page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    $("#header").load("header.html");
-    $("#footer").load("footer.html").delay("200");
-    $("body").removeClass('.wrapper').fadeIn("1000");
+    try {
+        $("#header").load("header.html");
+        $("#footer").load("footer.html").delay("200");
+        $("body").removeClass('.wrapper').fadeIn("1000");
+    } finally {
+        $("#header").load("../header.html");
+        $("#footer").load("../footer.html").delay("200");
+        $("body").removeClass('.wrapper').fadeIn("1000");
+    }
 });
