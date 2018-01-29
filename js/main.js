64a4fcee2774c01e68faf9acd72e0ac21a16d158
Spencer Gunning <swcgunning@gmail.com>
2018-01-29 18:33:09 -0500

    [2018r20] Various hotfixes for live staging test

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-    try {
-        $("#header").load("header.html");
-        $("#footer").load("footer.html").delay("200");
-        $("body").removeClass('.wrapper').fadeIn("1000");
-    } finally {
-        $("#header").load("../header.html");
-        $("#footer").load("../footer.html").delay("200");
-        $("body").removeClass('.wrapper').fadeIn("1000");
-    }
+    $("#header").load("../header.html");
+    $("#footer").load("../footer.html").delay("200");
+    $("body").removeClass('.wrapper').fadeIn("1000");
 });
